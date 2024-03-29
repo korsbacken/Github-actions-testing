a956562b7152c90c6e63303fb9ef3132aa193dca
korsb <korsbacken@gmail.com>
2024-03-29 17:50:26 +0100

    remove include in pom

--- src/test/java/com/example/CucumberSelenium/runner/CucumberTestRunner.java
+++ src/test/java/com/example/CucumberSelenium/runner/CucumberTestRunner.java
@@ -3,9 +3,8 @@ package com.example.CucumberSelenium.runner;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/java/com/example/CucumberSelenium/resources/features", glue = "com.example.CucumberSelenium.stepdefs")
-public class CucumberTestRunnerTest {
+public class CucumberTestRunner {
 }
